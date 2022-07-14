4c147efa73a963dfd0914a5672cd652264dea011
meysam <mysm.shahbazi@gmail.com>
2022-07-14 10:23:16 -0400

    add logs

--- commands.sh
+++ commands.sh
@@ -1,8 +1,19 @@
-#
+#!/usr/bin/sh
 # initiall project with git
 git init
 code README.md
 
+git add 
+git commit README.md -m "first commit"
+
+ls -al
+# for get rid of git! 
+rm -rf .git/ 
+
+git init .
+
+
+git log 
 
 
 
